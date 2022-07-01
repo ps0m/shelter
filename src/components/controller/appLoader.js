a2704d8b57b2d8a8c058b849143fdef1885d090f
ps0m <ps0m@tut.by>
2022-07-01 13:19:52 +0300

    feat: add API key, typescript and setting up configurations

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
   constructor() {
     super('https://newsapi.org/v2/', {
-      apiKey: '', // получите свой ключ https://newsapi.org/
+      apiKey: 'eda60b513a1d4833927372bd6ef8f44a', // получите свой ключ https://newsapi.org/
     });
   }
 }
