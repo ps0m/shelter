bee2eefca1df36615eb7efc547841800aab4386f
ps0m <ps0m@tut.by>
2022-07-15 23:30:22 +0300

    feat: add MySelect module

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import GeterCards from "./API/GeterCards";
 import MyHeader from './components/UI/MyHeader/MyHeader';
 import CardList from './components/CardList';
 import MyFooter from './components/UI/MyFooter/MyFooter';
+import MySelect from './components/UI/MySelect/MySelect';
 
 const App = () => {
   const [cards, setCards] = useState<ICard[]>([]);
   const [equalPurchase, setEqualPurchase] = useState<number>(0);
+  const [selectedSort, setSelectedSort] = useState<keyof ICard>('name');
 
   useEffect(() => {
     getCards();
@@ -23,10 +25,24 @@ const App = () => {
     setEqualPurchase(equal);
   }
 
+  const sortCards = (sort: keyof ICard) => {
+    console.log(sort);
+    setSelectedSort(sort);
+    setCards([...cards].sort((a, b) => a[sort].localeCompare(b[sort])))
+  }
 
   return (
-    <div className=".container" >
+    <div className="container" >
       <MyHeader purchase={equalPurchase} />
+      <MySelect
+        defaultValue={'Сортировать по:'}
+        options={[
+          { value: "name", name: 'По имени' },
+          { value: "equal", name: 'По количеству' }
+        ]}
+        value={selectedSort}
+        onChange={sortCards}
+      />
       <CardList cards={cards} put={putInBasket} />
       <MyFooter />
     </div>
